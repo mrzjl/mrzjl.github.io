b9fea6a143cde1a8f0424c35f888c21709e6ebca
Jiale <mr_jiale@outlook.com>
2015-10-20 20:14:08 +0800

    remove some animation

--- static/js/index.js
+++ static/js/index.js
@@ -21,7 +21,6 @@ $(document).ready(function() {
  * 固定底栏的初始化方法
  * 在一开始载入页面时，使用fixFooter()方法固定底栏。
  * 在浏览器窗口改变大小是，依然固定底栏
- * @return {[type]} [description]
  */
 function fixFooterInit() {
     var footerHeight = $('footer').outerHeight();
@@ -30,15 +29,9 @@ function fixFooterInit() {
 
     fixFooter(footerHeight, footerMarginTop); //fix footer at the beginning
 
-    $(window).resize(function() { //when resize window, footer can auto get the postion
+    $(window).resize(function() {
         fixFooter(footerHeight, footerMarginTop);
     });
-
-    /*    $('body').click(function() {
-        fixFooter(footerHeight, footerMarginTop);
-    });*/
-
-
 }
 
 /**
@@ -50,15 +43,6 @@ function fixFooterInit() {
 function fixFooter(footerHeight, footerMarginTop) {
     var windowHeight = $(window).height();
     var contentHeight = $('body>.container').outerHeight() + $('body>.container').offset().top + footerHeight + footerMarginTop;
-    // console.log("window---"+windowHeight);
-    // console.log("$('body>.container').outerHeight()---"+$('body>.container').outerHeight() );
-    // console.log("$('body>.container').height()---"+$('body>.container').height() );
-    // console.log("$('#main').height()--------"+$('#main').height());
-    // console.log("$('body').height()--------"+$('body').height());
-    //console.log("$('#main').html()--------"+$('#main').html());
-    // console.log("$('body>.container').offset().top----"+$('body>.container').offset().top);
-    // console.log("footerHeight---"+footerHeight);
-    // console.log("footerMarginTop---"+footerMarginTop);
     console.log(contentHeight);
     if (contentHeight < windowHeight) {
         $('footer').addClass('navbar-fixed-bottom');
