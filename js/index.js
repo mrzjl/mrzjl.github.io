2d744730df9fd91e6c843aaf453856a14729e25c
Jiale <mr_jiale@outlook.com>
2015-11-01 19:28:13 +0800

    sidebar categories

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
+    cateDisplay();
     generateContent();
     sidebarShow();
     smoothScroll();
+    scrollToTop();
 })
 
 
@@ -34,22 +36,63 @@ function smoothScroll(){
 }
 
 
-/*点击菜单键显示栏*/
-function sidebarShow(){
-    $("#menu-icon").click(function(){
-        $(".secondary:first").toggleClass("show-sidebar");
+/*categories*/
+function cateDisplay(){
+    var cate=$("#categories > ul > li");
+    cate.click(function(){
+        $("ul",this).css("display") == "block" ? $("ul",this).slideUp(200) : $("ul",this).slideDown(200);
         return false;
     });
 }
 
-function categoryDisplay() {
-    /*only show All*/
-    $('.primary>div[post-cate!=All]').hide();
-    /*show category when click categories list*/
-    $('.categories-list-item').click(function() {
-        var cate = $(this).attr('cate'); //get category's name
 
-        $('.post-list-body>div[post-cate!=' + cate + ']').hide(250);
-        $('.post-list-body>div[post-cate=' + cate + ']').show(400);
+//点击回到顶部
+function scrollToTop() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 100) {
+            $("#scroll-to-top").fadeIn(200);
+        } else {
+            $("#scroll-to-top").fadeOut(200);
+        }
     });
+    $("#scroll-to-top").click(function() {
+        $('html,body').animate({
+            scrollTop: 0
+        },
+        500);
+        return false;
+    });
+}
+
+
+/*滑动侧栏*/
+function sidebarShow(){
+    var sec=$(".secondary");
+    var secRight=$(".secondary").css("right");
+    var secWidth=$(".secondary").css("width");
+    $("#menu-icon").click(function(){
+        secRight=$(".secondary").css("right");
+        secWidth=$(".secondary").css("width");
+        if(secRight == "0px"){
+            $(".secondary").css("right","-"+secWidth);
+        }else{
+                $(".secondary").css("right","0");            
+        }
+        return false;
+    });
+
+    /*点击侧栏内链接时隐藏侧栏*/
+    $("a",sec).click(function(){
+        var notURL=new RegExp("\#$");//判断是否为锚链接
+        if(( $("#menu-icon").css("display") == "block" ) && !(notURL.test(this))){
+            $(".secondary").css("right","-"+secWidth);
+        }
+    });
+    /*点击外部隐藏侧栏*/
+    $("*",".primary").click(function(){
+        if( $("#menu-icon").css("display") == "block" ){
+            $(".secondary").css("right","-"+secWidth);
+        }
+    });
+
 }
